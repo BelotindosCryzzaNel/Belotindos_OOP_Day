dbddb0253a99800920fcf3137068dc28a5a97325
BelotindosCryzzaNel <159901477+BelotindosCryzzaNel@users.noreply.github.com>
2024-03-12 14:58:44 +0800

    Add files via upload

---
+++ Belotindos_LiveAct/src/LiveAct/Paladin.java
@@ -0,0 +1,36 @@
+
+package LiveAct;
+
+public class Paladin extends Swordsman {
+    private boolean hasResurrected;
+    
+    public boolean isHasResurrected(){
+        return hasResurrected;
+    }
+    public Paladin(){
+        super();
+        boolean hasResurrected = false;
+    }
+    
+    
+    public void receiveDamage(int damage){
+        if (damage % 2 == 0){
+            damage /=2;
+        }
+        super.receivedamage(damage);
+        
+        if (getHealth()<=0){
+            resurrect();
+        }
+    }
+    
+    public void resurrect(){
+        if (hasResurrected){
+            hasResurrected = true;
+            setHealth(100);
+        } else {
+            System.out.println("Paladin has died");
+        }
+    }
+    
+}
---
+++ Belotindos_LiveAct/src/LiveAct/Swordsman.java
@@ -0,0 +1,12 @@
+
+package LiveAct;
+
+
+
+public class Swordsman extends Character {
+    
+    public Swordsman(){
+        super(100, 10, 10);
+    }
+    
+}
